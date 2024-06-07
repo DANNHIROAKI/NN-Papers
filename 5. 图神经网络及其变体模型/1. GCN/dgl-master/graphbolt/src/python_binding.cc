5e3b5c69e4d2055a0f64487873e783de5249a061
DUAN <134042675+DANNHIROAKI@users.noreply.github.com>
2024-06-07 11:38:29 +1000

    xxxx

---
+++ 5. 图神经网络及其变体模型/1. GCN/dgl-master/graphbolt/src/python_binding.cc
@@ -0,0 +1,74 @@
+/**
+ *  Copyright (c) 2023 by Contributors
+ * @file python_binding.cc
+ * @brief Graph bolt library Python binding.
+ */
+
+#include <graphbolt/fused_csc_sampling_graph.h>
+#include <graphbolt/isin.h>
+#include <graphbolt/serialize.h>
+#include <graphbolt/unique_and_compact.h>
+
+#include "./index_select.h"
+
+namespace graphbolt {
+namespace sampling {
+
+TORCH_LIBRARY(graphbolt, m) {
+  m.class_<FusedSampledSubgraph>("FusedSampledSubgraph")
+      .def(torch::init<>())
+      .def_readwrite("indptr", &FusedSampledSubgraph::indptr)
+      .def_readwrite("indices", &FusedSampledSubgraph::indices)
+      .def_readwrite(
+          "original_row_node_ids", &FusedSampledSubgraph::original_row_node_ids)
+      .def_readwrite(
+          "original_column_node_ids",
+          &FusedSampledSubgraph::original_column_node_ids)
+      .def_readwrite(
+          "original_edge_ids", &FusedSampledSubgraph::original_edge_ids)
+      .def_readwrite("type_per_edge", &FusedSampledSubgraph::type_per_edge);
+  m.class_<FusedCSCSamplingGraph>("FusedCSCSamplingGraph")
+      .def("num_nodes", &FusedCSCSamplingGraph::NumNodes)
+      .def("num_edges", &FusedCSCSamplingGraph::NumEdges)
+      .def("csc_indptr", &FusedCSCSamplingGraph::CSCIndptr)
+      .def("indices", &FusedCSCSamplingGraph::Indices)
+      .def("node_type_offset", &FusedCSCSamplingGraph::NodeTypeOffset)
+      .def("type_per_edge", &FusedCSCSamplingGraph::TypePerEdge)
+      .def("edge_attributes", &FusedCSCSamplingGraph::EdgeAttributes)
+      .def("set_csc_indptr", &FusedCSCSamplingGraph::SetCSCIndptr)
+      .def("set_indices", &FusedCSCSamplingGraph::SetIndices)
+      .def("set_node_type_offset", &FusedCSCSamplingGraph::SetNodeTypeOffset)
+      .def("set_type_per_edge", &FusedCSCSamplingGraph::SetTypePerEdge)
+      .def("set_edge_attributes", &FusedCSCSamplingGraph::SetEdgeAttributes)
+      .def("in_subgraph", &FusedCSCSamplingGraph::InSubgraph)
+      .def("sample_neighbors", &FusedCSCSamplingGraph::SampleNeighbors)
+      .def(
+          "sample_negative_edges_uniform",
+          &FusedCSCSamplingGraph::SampleNegativeEdgesUniform)
+      .def("copy_to_shared_memory", &FusedCSCSamplingGraph::CopyToSharedMemory)
+      .def_pickle(
+          // __getstate__
+          [](const c10::intrusive_ptr<FusedCSCSamplingGraph>& self)
+              -> torch::Dict<
+                  std::string, torch::Dict<std::string, torch::Tensor>> {
+            return self->GetState();
+          },
+          // __setstate__
+          [](torch::Dict<std::string, torch::Dict<std::string, torch::Tensor>>
+                 state) -> c10::intrusive_ptr<FusedCSCSamplingGraph> {
+            auto g = c10::make_intrusive<FusedCSCSamplingGraph>();
+            g->SetState(state);
+            return g;
+          });
+  m.def("from_fused_csc", &FusedCSCSamplingGraph::FromCSC);
+  m.def("load_fused_csc_sampling_graph", &LoadFusedCSCSamplingGraph);
+  m.def("save_fused_csc_sampling_graph", &SaveFusedCSCSamplingGraph);
+  m.def(
+      "load_from_shared_memory", &FusedCSCSamplingGraph::LoadFromSharedMemory);
+  m.def("unique_and_compact", &UniqueAndCompact);
+  m.def("isin", &IsIn);
+  m.def("index_select", &ops::IndexSelect);
+}
+
+}  // namespace sampling
+}  // namespace graphbolt
---
+++ 5. 图神经网络及其变体模型/2. DCNN/dcnn-master/client/run.py
@@ -0,0 +1,153 @@
+import numpy as np
+from sklearn import metrics
+import random
+
+import parser
+
+from python import data
+from python import models
+from python import params as params_mod
+
+
+metadata = {
+    "cora": {
+        "parser": data.parse_cora,
+        "num_nodes": 2708,
+        "num_features": 1433,
+        "num_classes": 7,
+    },
+    "sparse_cora": {
+        "parser": data.parse_cora_sparse,
+        "num_nodes": 2708,
+        "num_features": 1433,
+        "num_classes": 7,
+    },
+    'nci1': {
+        "parser": lambda: data.parse_graph_data('nci1.graph'),
+        "num_nodes": None,
+        "num_graphs": None,
+        "num_features": 37,
+        "num_classes": 2,
+    }
+
+}
+
+model_map = {
+    "node_classification": models.NodeClassificationDCNN,
+    "deep_node_classification": models.DeepNodeClassificationDCNN,
+    "post_sparse_node_classification": models.PostSparseNodeClassificationDCNN,
+    "pre_sparse_node_classification": models.PreSparseNodeClassificationDCNN,
+    "true_sparse_node_classification": models.TrueSparseNodeClassificationDCNN,
+    "deep_dense_node_classification": models.DeepDenseNodeClassificationDCNN,
+    "graph_classification": models.GraphClassificationDCNN,
+    "deep_graph_classification": models.DeepGraphClassificationDCNN,
+    "deep_graph_classification_with_reduction": models.DeepGraphClassificationDCNNWithReduction,
+    "deep_graph_classification_with_kron_reduction": models.DeepGraphClassificationDCNNWithKronReduction,
+    "feature_aggregated_graph_classification": models.GraphClassificationFeatureAggregatedDCNN,
+}
+
+hyperparameter_choices = {
+    "num_hops": range(1, 6),
+    "learning_rate": [0.01, 0.05, 0.1, 0.25],
+    "optimizer": params_mod.update_map.keys(),
+    "loss": params_mod.loss_map.keys(),
+    "dcnn_nonlinearity": params_mod.nonlinearity_map.keys(),
+    "dense_nonlinearity": params_mod.nonlinearity_map.keys(),
+    "num_epochs": [10, 20, 50, 100],
+    "batch_size": [10, 100],
+    "early_stopping": [0, 1],
+    "stop_window_size": [1, 5, 10],
+    "num_dcnn_layers": range(2, 6),
+    "num_dense_layers": range(2, 6),
+}
+
+def run_node_classification(parameters):
+    A, X, Y = metadata[parameters.data]["parser"]()
+
+    dcnn = model_map[parameters.model](parameters, A)
+
+    num_nodes = A.shape[0]
+
+    indices = np.arange(num_nodes).astype('int32')
+    np.random.shuffle(indices)
+
+    train_indices = indices[:num_nodes // 3]
+    valid_indices = indices[num_nodes // 3: (2 * num_nodes) // 3]
+    test_indices = indices[(2 * num_nodes) // 3:]
+
+    dcnn.fit(X, Y, train_indices, valid_indices)
+
+    predictions = dcnn.predict(X, test_indices)
+    actuals = Y[test_indices, :].argmax(1)
+
+    accuracy = metrics.accuracy_score(actuals, predictions)
+
+    print "Test Accuracy: %.4f" % (accuracy,)
+
+def run_graph_classification(params):
+    print "parsing data..."
+    A, X, Y = metadata[parameters.data]["parser"]()
+
+    # Shuffle the data.
+    tmp = list(zip(A, X, Y))
+    random.shuffle(tmp)
+    A, X, Y = zip(*tmp)
+
+    num_graphs = len(A)
+
+    indices = np.arange(num_graphs).astype('int32')
+    np.random.shuffle(indices)
+
+    train_indices = indices[:num_graphs // 3]
+    valid_indices = indices[num_graphs // 3: (2 * num_graphs) // 3]
+    test_indices = indices[(2 * num_graphs) // 3:]
+
+    print "initializing model..."
+    m = model_map[params.model](params)
+
+    print "training model..."
+    m.fit(A, X, Y, train_indices=train_indices, valid_indices=valid_indices)
+
+    test_predictions = []
+    test_actuals = []
+    for test_index in test_indices:
+        pred = m.predict(A[test_index], X[test_index])
+        test_predictions.append(pred)
+        test_actuals.append(Y[test_index].argmax())
+
+    test_accuracy = 0.0
+    num_test = len(test_predictions)
+    for i in range(len(test_predictions)):
+        if (test_predictions[i] == test_actuals[i]):
+            test_accuracy += 1.0
+
+    test_accuracy /= num_test
+    print "Test Accuracy: %.6f" % (test_accuracy,)
+    print params
+    print "RESULTS:%.6f" % test_accuracy
+    print "done"
+
+if __name__ == '__main__':
+    parameters = parser.parse()
+
+    if parameters.explore:
+        while True:
+            for possibility in hyperparameter_choices.keys():
+                choice = random.choice(hyperparameter_choices[possibility])
+                parameters.set(possibility, choice)
+
+            print "Parameter choices:"
+            print parameters
+
+            try:
+                if "node_classification" in parameters.model:
+                    run_node_classification(parameters)
+                else:
+                    run_graph_classification(parameters)
+            except ValueError:
+                print "Encountered nan loss, trying again."
+
+    if "node_classification" in parameters.model:
+        run_node_classification(parameters)
+    else:
+        run_graph_classification(parameters)
