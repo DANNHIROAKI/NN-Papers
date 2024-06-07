d9bae336f4414b35f00f053ac786ab9f8341e4fd
DUAN <134042675+DANNHIROAKI@users.noreply.github.com>
2024-06-07 11:43:56 +1000

    xxxxx

---
+++ 4. Transformer及其变体模型/18.Performer/google-research-master/dense_representations_for_entity_retrieval/mel/mewsli-9/get_wikinews_dumps.sh
@@ -0,0 +1,46 @@
+# Copyright 2023 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Download WikiNews dumps from archive.org
+#
+# Usage: get_wikinews_dumps.sh [output_dir]
+
+set -eux
+
+DEFAULT_OUTDIR="$(dirname $0)/output/download"
+OUTDIR="${1:-${DEFAULT_OUTDIR}}"
+
+# Snapshot date and languages to target.
+DATE="20190101"
+LANG_LIST=(ar de en es fa ja sr ta tr)
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+CHECKSUMS="${SCRIPT_DIR}/dump_checksums.txt"
+CHECKSUM_TOOL="${SCRIPT_DIR}/../tools/md5_check.sh"
+
+mkdir -p "${OUTDIR}"
+
+for lang in ${LANG_LIST[@]}; do
+  echo ">Download '${lang}'..."
+  filename="${lang}wikinews-${DATE}-pages-articles.xml.bz2"
+  url="https://archive.org/download/${lang}wikinews-${DATE}/${filename}"
+  wget -a "${OUTDIR}/wget.log" -O "${OUTDIR}/${filename}" "${url}"
+done
+
+echo ">Verify..."
+pushd "${OUTDIR}"
+bash "${CHECKSUM_TOOL}" "${CHECKSUMS}"
+popd
+
+echo
+echo ">Done: ${OUTDIR}"
---
+++ 4. Transformer及其变体模型/18.Performer/google-research-master/scann/scann/distance_measures/many_to_many/fp8_transposed.cc
@@ -0,0 +1,96 @@
+// Copyright 2023 The Google Research Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "scann/distance_measures/many_to_many/fp8_transposed.h"
+
+#include <algorithm>
+#include <cstdint>
+
+#include "scann/utils/intrinsics/flags.h"
+#include "scann/utils/types.h"
+
+namespace research_scann {
+
+namespace {
+uint8_t SimdBlockSize() {
+  if (RuntimeSupportsAvx512()) {
+    return 16;
+  } else if (RuntimeSupportsAvx1()) {
+    return 8;
+  } else if (RuntimeSupportsSse4()) {
+    return 4;
+  } else {
+    return 1;
+  }
+}
+}  // namespace
+
+FP8SimdBlockTransposedDatabase::FP8SimdBlockTransposedDatabase()
+    : dimensionality_(0), simd_block_size_(SimdBlockSize()) {}
+
+FP8SimdBlockTransposedDatabase::FP8SimdBlockTransposedDatabase(
+    const DenseDataset<int8_t>& db, ConstSpan<float> inverse_fp8_multipliers)
+    : FP8SimdBlockTransposedDatabase(db, SimdBlockSize(),
+                                     inverse_fp8_multipliers) {}
+
+FP8SimdBlockTransposedDatabase::FP8SimdBlockTransposedDatabase(
+    const DenseDataset<int8_t>& db, uint8_t simd_block_size,
+    ConstSpan<float> inverse_fp8_multipliers)
+    : FP8SimdBlockTransposedDatabase(db.data(), db.dimensionality(),
+                                     simd_block_size, inverse_fp8_multipliers) {
+}
+
+FP8SimdBlockTransposedDatabase::FP8SimdBlockTransposedDatabase(
+    ConstSpan<int8_t> datapoint_major, DimensionIndex dimensionality,
+    ConstSpan<float> inverse_fp8_multipliers)
+    : FP8SimdBlockTransposedDatabase(datapoint_major, dimensionality,
+                                     SimdBlockSize(), inverse_fp8_multipliers) {
+}
+
+FP8SimdBlockTransposedDatabase::FP8SimdBlockTransposedDatabase(
+    ConstSpan<int8_t> datapoint_major, DimensionIndex dimensionality,
+    uint8_t simd_block_size, ConstSpan<float> inverse_fp8_multipliers)
+    : payload_(new int8_t[datapoint_major.size()]),
+      inverse_fp8_multipliers_(inverse_fp8_multipliers.data()),
+      size_(datapoint_major.size() / dimensionality),
+      dimensionality_(dimensionality),
+      simd_block_size_(simd_block_size) {
+  CHECK_EQ(0, datapoint_major.size() % dimensionality_);
+  if (!inverse_fp8_multipliers.empty()) {
+    CHECK_EQ(dimensionality_, inverse_fp8_multipliers.size());
+  }
+
+  const int8_t* untransposed_ptr = datapoint_major.data();
+  for (DatapointIndex block_start = 0; block_start < size_;
+       block_start += simd_block_size_) {
+    const DatapointIndex block_size =
+        std::min<DatapointIndex>(simd_block_size_, size_ - block_start);
+    TransposeOneBlock(untransposed_ptr + block_start * dimensionality_,
+                      block_size,
+                      payload_.get() + block_start * dimensionality_);
+  }
+}
+
+void FP8SimdBlockTransposedDatabase::TransposeOneBlock(const int8_t* src,
+                                                       size_t block_size,
+                                                       int8_t* dest) {
+  for (DatapointIndex dp_idx : Seq(block_size)) {
+    const int8_t* dp_start = src + dimensionality_ * dp_idx;
+    for (DimensionIndex dim_idx : Seq(dimensionality_)) {
+      dest[dim_idx * block_size + dp_idx] = dp_start[dim_idx];
+    }
+  }
+}
+
+}  // namespace research_scann
